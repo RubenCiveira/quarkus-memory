b39bac29caf5c201d7a22a6d9ed68edf8bde2168
Ruben civeira <rubenciveira@gmail.com>
2024-11-25 16:04:21 +0100

    compilacion nativa en git

--- native-dock.sh
+++ native-dock.sh
@@ -1,3 +1,5 @@
-./mvnw clean package -Dnative -DskipTests -Dquarkus.native.container-runtime=docker -Dquarkus.native.container-build=true
-docker build -f src/main/docker/Dockerfile.native-micro -t quarkus/code-with-quarkus-native-micro .
-docker run -i --rm -p 8080:8080 quarkus/code-with-quarkus-native-micro
+# ./mvnw clean package -Dnative -DskipTests -Dquarkus.native.container-runtime=docker -Dquarkus.native.container-build=true
+# docker build -f src/main/docker/Dockerfile.native-micro -t quarkus/code-with-quarkus-native-micro .
+
+docker build -f src/main/docker/Dockerfile.native-micro.multistage --no-cache -t quarkus/code-with-quarkus-native-micro .
+# docker run -i --rm -p 8080:8080 quarkus/code-with-quarkus-native-micro
