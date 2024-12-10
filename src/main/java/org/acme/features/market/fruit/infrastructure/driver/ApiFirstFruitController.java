ca989427ada11f3e1afdf69a4a0d15af2529c312
Ruben civeira <rubenciveira@gmail.com>
2024-12-10 06:08:12 +0100

    feat: params and cursor on open api

--- src/main/java/org/acme/features/market/fruit/infrastructure/driver/ApiFirstFruitController.java
+++ src/main/java/org/acme/features/market/fruit/infrastructure/driver/ApiFirstFruitController.java
@@ -1,11 +1,12 @@
 package org.acme.features.market.fruit.infrastructure.driver;
 
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-
 import org.acme.common.security.Actor;
 import org.acme.common.security.Connection;
+import org.acme.features.market.fruit.application.interaction.FruitDto;
 import org.acme.features.market.fruit.application.interaction.query.FruitListQuery;
 import org.acme.features.market.fruit.application.interaction.result.FruitListResult;
 import org.acme.features.market.fruit.application.usecase.ListFruitUsecase;
@@ -13,7 +14,8 @@ import org.acme.features.market.fruit.domain.gateway.FruitCursor;
 import org.acme.features.market.fruit.domain.gateway.FruitFilter;
 import org.acme.openapi.api.FruitApi;
 import org.acme.openapi.model.Fruit;
-
+import org.acme.openapi.model.FruitApiList200Response;
+import org.acme.openapi.model.FruitApiList200ResponseNext;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.ws.rs.core.Response;
 
@@ -45,17 +47,22 @@ public class ApiFirstFruitController implements FruitApi {
   }
 
   @Override
-  public Response fruitApiList(Integer limit) {
+  public Response fruitApiList(Integer limit, String sinceUid) {
     FruitFilter.FruitFilterBuilder filter = FruitFilter.builder();
     FruitCursor.FruitCursorBuilder cursor = FruitCursor.builder();
     cursor = cursor.limit(limit);// == limit ? 10 : limit);
+    cursor.sinceUid( sinceUid );
     Actor actor = new Actor();
     Connection connection = new Connection();
     FruitListResult list = fruits.list(FruitListQuery.builder().actor(actor).connection(connection)
         .filter(filter.build()).cursor(cursor.build()).build());
     // .map(items -> Response.ok(items.getFruits()).build()).await().atMost(Duration.ofSeconds(2));
     try {
-      return Response.ok(list.getFruits().get(1, TimeUnit.SECONDS)).build();
+      FruitApiList200Response res = new FruitApiList200Response();
+      List<FruitDto> fruits = list.getFruits().get(1, TimeUnit.SECONDS);
+      res.setContent( toApiModel(fruits) );
+      res.setNext( next(fruits) );
+      return Response.ok(res).build();
     } catch (InterruptedException | ExecutionException | TimeoutException e) {
       e.printStackTrace();
       return Response.serverError().build();
@@ -79,4 +86,26 @@ public class ApiFirstFruitController implements FruitApi {
     return null;
   }
 
+  public FruitApiList200ResponseNext next(List<FruitDto> list) {
+    if( list.isEmpty() ) {
+      return null;
+    } else {
+      FruitApiList200ResponseNext next = new FruitApiList200ResponseNext();
+      FruitDto last = list.get(list.size()-1);
+      next.setSinceUid( last.getUid() );
+      return next;
+    }
+  }
+  
+  public List<Fruit> toApiModel(List<FruitDto> fruit) {
+    return fruit.stream().map(this::toApiModel).toList();
+  }
+
+  public Fruit toApiModel(FruitDto dto) {
+    Fruit fruit = new Fruit();
+    fruit.setName( dto.getName() );
+    fruit.setUid( dto.getUid() );
+    fruit.setVersion( dto.getVersion() );
+    return fruit;
+  }
 }
