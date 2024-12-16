aae06f595e9250ae22e5b440d275840271523256
Ruben civeira <rubenciveira@gmail.com>
2024-12-16 09:30:29 +0100

    feat: add sub where blocks

--- src/main/java/org/acme/common/sql/SchematicQuery.java
+++ src/main/java/org/acme/common/sql/SchematicQuery.java
@@ -1,9 +1,79 @@
 package org.acme.common.sql;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
 public class SchematicQuery {
+  public static class Partial {
+    public static Partial or(Function<Partial, List<Partial>> p) {
+      Partial partial = new Partial("or", "");
+      p.apply(partial);
+      return partial;
+    }
+    
+    public static Partial and(Function<Partial, List<Partial>> p) {
+      Partial partial = new Partial("and", "");
+      p.apply(partial);
+      return partial;
+    }
+    
+    public static Partial not(Function<Partial, List<Partial>> p) {
+      Partial partial = new Partial("and", "not");
+      p.apply(partial);
+      return partial;
+    }
+    
+    private final String join;
+    private final String converter;
+    List<Object[]> params = new ArrayList<>();
+    
+    
+    private Partial(String join, String converter) {
+      this.join = join;
+      this.converter = converter;
+    }
+    
+    public Partial where(String on, String field, SqlOperator operator, SqlParameterValue value) {
+      params.add(new Object[] { on, field, operator, value });
+      return this;
+    }
+    
+    public Partial where(String field, SqlOperator operator, SqlParameterValue value) {
+      this.where(null, field, operator, value);
+      return this;
+    }
+    
+    public Partial where(Partial partial) {
+      params.add(new Object[] { partial });
+      return this;
+    }
+    
+    public String append(int prefix, SchematicQuery parametrized) {
+      List<String> wheres = new ArrayList<>();
+      for (Object[] param : params) {
+        if( param.length == 1 ) {
+          String where = ((Partial)param[0]).append( ++prefix, parametrized ) ;
+          prefix += where.length();
+          wheres.add( where );
+        } else {
+          String on = (String)param[0];
+          String field = (String)param[1];
+          SqlOperator operator = (SqlOperator)param[2];
+          SqlParameterValue value = (SqlParameterValue)param[3];
+          
+          String name = "_field_" + (++prefix);
+          parametrized.parametrized.with(name, value);
+          wheres.add( (null==on?"":parametrized.escape(on) + "." ) + parametrized.escape(field) + " " + operator.value + " :" + name );
+        }
+      }
+      return " " + converter + " ( " + String.join(" " + join + " " , wheres) + " )";
+    }
+  }
+  
+  
   private final String type;
   private final String table;
   private final AbstractSqlParametrized<?> parametrized;
@@ -63,11 +133,18 @@ public class SchematicQuery {
     set.put(escape(field), ":" + name);
   }
 
-  public void where(String field, SqlOperator operator, SqlParameterValue value) {
+  public void where(Partial partial) {
+    where.append(" and " + partial.append(where.length(), this));
+  }
+  
+  public void where(String on, String field, SqlOperator operator, SqlParameterValue value) {
     String name = "_field_" + where.length();
-    where.append(" and " + escape(field) + " " + operator.value + " :" + name);
+    where.append(" and " + (null==on?"":escape(on) + "." ) + escape(field) + " " + operator.value + " :" + name);
     parametrized.with(name, value);
-
+  }
+  
+  public void where(String field, SqlOperator operator, SqlParameterValue value) {
+    where(null, field, operator, value);
   }
 
   public void orderAsc(String field) {
--- src/main/java/org/acme/common/sql/SqlSchematicQuery.java
+++ src/main/java/org/acme/common/sql/SqlSchematicQuery.java
@@ -1,10 +1,25 @@
 package org.acme.common.sql;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import org.acme.common.sql.SchematicQuery.Partial;
 
 public final class SqlSchematicQuery<T> extends AbstractSqlQuery<T, SqlSchematicQuery<T>> {
   private final SchematicQuery query;
-
+  
+  public static Partial or(Function<Partial, List<Partial>> p) {
+    return Partial.or(p);
+  }
+  
+  public static Partial and(Function<Partial, List<Partial>> p) {
+    return Partial.and(p);
+  }
+  
+  public static Partial not(Function<Partial, List<Partial>> p) {
+    return Partial.not(p);
+  }
+  
   public SqlSchematicQuery(SqlTemplate template, String table) {
     super(template);
     this.query = new SchematicQuery("SELECT", table, this);
@@ -30,8 +45,18 @@ public final class SqlSchematicQuery<T> extends AbstractSqlQuery<T, SqlSchematic
     return this;
   }
 
+  public SqlSchematicQuery<T> where(String on, String string, SqlOperator gt, SqlParameterValue of) {
+    this.query.where(on, string, gt, of);
+    return this;
+  }
+  
   public SqlSchematicQuery<T> where(String string, SqlOperator gt, SqlParameterValue of) {
     this.query.where(string, gt, of);
+    return this;
+  }
+  
+  public SqlSchematicQuery<T> where(Partial partial) {
+    this.query.where(partial);
     return this;
   }
 }
