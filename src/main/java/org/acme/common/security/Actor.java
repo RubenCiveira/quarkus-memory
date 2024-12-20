71334322202695d05ca8e9eff33637ea75a39cc7
Ruben civeira <rubenciveira@gmail.com>
2024-12-20 15:07:15 +0100

    feat: delegate rest comunitacion with current request

--- src/main/java/org/acme/common/security/Actor.java
+++ src/main/java/org/acme/common/security/Actor.java
@@ -1,5 +1,32 @@
 package org.acme.common.security;
 
-public class Actor {
+import java.util.List;
+import java.util.Optional;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class Actor {
+  private final String name;
+  private final String tenant;
+  private final boolean autenticated;
+  private final List<String> roles;
+
+  public Optional<String> getTenant() {
+    return Optional.ofNullable(tenant);
+  }
+
+  public Optional<String> getName() {
+    return Optional.ofNullable(name);
+  }
+
+  public boolean isAutenticated() {
+    return autenticated;
+  }
+
+  public boolean hasAnyRole(String... strings) {
+    return true;
+  }
 }
--- src/main/java/org/acme/common/security/Connection.java
+++ src/main/java/org/acme/common/security/Connection.java
@@ -1,5 +1,33 @@
 package org.acme.common.security;
 
+import java.time.ZonedDateTime;
+import java.util.Locale;
+import java.util.Optional;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+
+@Getter
+@Builder
 public class Connection {
+  private final ZonedDateTime startTime = ZonedDateTime.now();
+  private final Locale locale;
+  @NonNull
+  private final String request;
+  private final String remote;
+  private final String remoteApplication;
+
+  public Optional<String> getRemoteApplication() {
+    return Optional.ofNullable(remoteApplication);
+  }
+
+  public Optional<String> getRemote() {
+    return Optional.ofNullable(remote);
+  }
+
+  public Locale getLocale() {
+    return locale == null ? locale : Locale.getDefault();
+  }
 
 }
