b18c569668b28ca670a228f815b6ef6178e983fa
Ruben civeira <rubenciveira@gmail.com>
2025-02-20 22:31:47 +0100

    test: traze display

--- src/main/java/org/acme/bootstrap/graphql/GraphQLService.java
+++ src/main/java/org/acme/bootstrap/graphql/GraphQLService.java
@@ -10,7 +10,8 @@ import org.acme.common.projection.ExecutionTree;
 import org.acme.common.projection.ProjectionRunner;
 import org.acme.common.projection.SelfProjection;
 import org.acme.features.market.area.infrastructure.bootstrap.AreaProjectionDescriptor.AreaExecutionPlanner;
-
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.Tracer;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -19,31 +20,42 @@ import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.UriInfo;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Path("/test/graphql")
 @RequiredArgsConstructor
 public class GraphQLService {
 
   private final ProjectionRunner runner;
   private final SelfProjection self;
+  private final Tracer tracer;
 
   @GET()
   public Object testQl(@Context HttpHeaders httpHeaders, @Context UriInfo info) {
+    log.warn("Entrada de control en el log");
     Map<String, String> query = info.getQueryParameters().entrySet().stream()
         .collect(Collectors.toMap(Map.Entry::getKey, entry -> String.join(",", entry.getValue())));
 
     ExecutionTree tree = self.getExecutionTree();
+
+    Span span = tracer.spanBuilder("My custom span")
+        .setAttribute("attr", "Colorines de aqui")
+        .setParent(io.opentelemetry.context.Context.current().with(Span.current()))
+        .startSpan();
     ExecutionPlan plan = ExecutionPlan.builder().path("/api/market/areas").tree(tree)
         .selection(new AreaExecutionPlanner().withUid("codigo").withName("nombre")
             .withPlace(ofPlace -> ofPlace.withName("place.name").withMerchant("jefe",
                 ofMerchant -> ofMerchant.withName("merchant.name")))
             .build())
         .build();
+    log.warn("Entrada dentro de un contexto hijo en el log");
 
     List<Gon> list = runner.list(plan, Gon.class, query, httpHeaders.getRequestHeaders());
     list.forEach(gon -> {
       System.out.println("TENGO UN " + gon.getCodigo());
     });
+    span.end();
     return list;
   }
 
